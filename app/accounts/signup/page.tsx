84589e55456459433b1b796276cdbb76f477743c
Chukwemeka Maduekwe <maduekwepedro@gmail.com>
2025-03-06 16:18:11 +0100

    fix(chore): move accounts layout to /accounts/layout from /layouts/accounts to improve readability

--- app/accounts/signup/page.tsx
+++ app/accounts/signup/page.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from "next";
+
 import dynamic from "next/dynamic";
 
 const ComingSoon = dynamic(() => import("@components/shared/coming-soon/ComingSoonContainer"));
+
+export const metadata: Metadata = {
+  title: "Wave Research: Sign Up",
+  keywords: ["signup", "register", "soccer manager", "soccer", "waverd", "football manager", "football"],
+  description:
+    "Embark on an exciting Soccer journey without limit! Register for Wave Research to enjoy premium features, and experience the best competitions in soccer and football data provider.",
+};
 
 const page = () => <ComingSoon finishDate={new Date("2025-04-01")} />;
 

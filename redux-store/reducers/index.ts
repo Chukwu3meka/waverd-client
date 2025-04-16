c7079783a2129b35225e81fc39a1d72891d3f198
Chukwuemeka Maduekwe <maduekwepedro@gmail.com>
2025-04-16 15:27:17 +0100

    refactor(redux): remove redundant redux store

--- redux-store/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { layoutReducer } from "./layout";
-import { accountReducer } from "./account";
-import { combineReducers } from "@reduxjs/toolkit";
-
-export const rootReducer = combineReducers({
-  layout: layoutReducer,
-  account: accountReducer,
-});
